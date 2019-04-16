87676f78e00abfe5f4a1d8eb484af52b61cbca73
Eric Feinstein <erichfeinstein@gmail.com>
2019-04-16 11:45:48 -0400

    sort functionality

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -8,11 +8,15 @@ import { ISession } from '../shared';
 export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[];
   @Input() filterBy: string;
+  @Input() sortBy: string;
   visibleSessions: ISession[];
 
   ngOnChanges() {
     if (this.sessions) {
       this.filterSessions(this.filterBy);
+      this.sortBy === 'name'
+        ? this.visibleSessions.sort(sortByNameAsc)
+        : this.visibleSessions.sort(sortByVotesAsc);
     }
   }
 
@@ -24,17 +28,26 @@ export class SessionListComponent implements OnChanges {
         this.visibleSessions = this.sessions.filter(session => {
           if (session.level === 'Beginner') return session;
         });
-      }
-      if (filterBy === 'intermediate') {
+      } else if (filterBy === 'intermediate') {
         this.visibleSessions = this.sessions.filter(session => {
           if (session.level === 'Intermediate') return session;
         });
-      }
-      if (filterBy === 'advanced') {
+      } else if (filterBy === 'advanced') {
         this.visibleSessions = this.sessions.filter(session => {
           if (session.level === 'Advanced') return session;
         });
       }
     }
   }
+}
+
+function sortByNameAsc(a, b) {
+  if (a.name < b.name) return -1;
+  if (a.name > b.name) return 1;
+  return 0;
+}
+function sortByVotesAsc(a, b) {
+  if (a.voters.length < b.voters.length) return -1;
+  if (a.voters.length > b.voters.length) return 1;
+  return 0;
 }
