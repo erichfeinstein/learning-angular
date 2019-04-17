aab7f2d14fccba99ba91c1f8794c2d9aa5148740
Eric Feinstein <erichfeinstein@gmail.com>
2019-04-17 14:34:00 -0400

    BUG FIX: bad import

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { IUser } from './user.model';
-import { HttpClient } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class AuthService {
